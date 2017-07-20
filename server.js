99fa423048fb802998f81fb2b87ae9934d25fcb5
Nimesh <nimesh.shrestha@hotmail.com>
2017-07-19 23:34:03 -0700

    changed to port 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
@@ -18,7 +18,6 @@ const io = socketIO(server, {
 });
 
 io.on('connection', (socket) => {
-	console.log("TEST")
   socket.on('unityClient', () => ClientManager.onUnityConnected(socket) )
   socket.on('webClient', (e) => ClientManager.onWebConnected(socket, e))
   //socket.on('disconnect', () => ClientManager.onDisconnect(socket));
